dd78f063908b1a5a558a6035ab16320b4eeb7893
scriptax <moradi.online@outlook.com>
2023-07-09 22:57:31 +0330

    updated content assets

---
+++ src/utils/icons.js
@@ -0,0 +1,11 @@
+import html from "./../assets/icons/html.svg";
+import css from "./../assets/icons/css.svg";
+import sass from "./../assets/icons/sass.svg";
+import javascript from "./../assets/icons/js.svg";
+import typescript from "./../assets/icons/ts.svg";
+import react from "./../assets/icons/react.svg";
+import tailwind from "./../assets/icons/tailwind.svg";
+import express from "./../assets/icons/express.svg";
+import nodejs from "./../assets/icons/nodejs.svg";
+
+export { html, css, sass, javascript, typescript, react, tailwind, nodejs, express };
---
+++ src/utils/pics.js
@@ -0,0 +1,6 @@
+import translateApp from "./../assets/images/translate-app.jpg";
+import weatherApp from "./../assets/images/weather-app.jpg";
+import noteApp from "./../assets/images/note-app.jpg";
+import stopWatchApp from "./../assets/images/stopwatch-app.jpg";
+
+export {translateApp, weatherApp, noteApp, stopWatchApp};
