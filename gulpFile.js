76107ecd1907e54dd48ef17a90d4e3d9f1457e72
scriptax <moradi.online@outlook.com>
2023-07-11 16:52:43 +0330

    update

--- gulpFile.js
+++ gulpFile.js
@@ -1,29 +1,31 @@
-const { series, src, dest, watch } = require('gulp');
-const purgecss = require('gulp-purgecss');
-const sass = require('gulp-sass')(require('sass'))
+const { series, src, dest, watch } = require("gulp");
+const purgecss = require("gulp-purgecss");
+const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src('./src/styles/index.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(purgecss({
-      content: ['./src/components/**/*.js'],
-      safelist: {
-        standard: [
-          /-primary-?/,
-          /-secondary-?/,
-          /-error-?/,
-          /-purple-?/,
-          /-info-?/,
-          /-blue-?/,
-          /-green-?/,
-        ]
-      }
-    }))
-    .pipe(dest('./src/styles/build'))
+  return src("./src/styles/index.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      purgecss({
+        content: ["./src/components/**/*.js"],
+        safelist: {
+          standard: [
+            /-primary-?/,
+            /-secondary-?/,
+            /-error-?/,
+            /-purple-?/,
+            /-info-?/,
+            /-blue-?/,
+            /-green-?/,
+          ],
+        },
+      }),
+    )
+    .pipe(dest("./src/styles/build"));
 }
 
 function watchTask() {
-  watch(['./src/styles/**/*.scss', './src/components/**/*.js'], buildStyles)
+  watch(["./src/styles/**/*.scss", "./src/components/**/*.js"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
--- src/components/navbar/Logo.js
+++ src/components/navbar/Logo.js
@@ -2,11 +2,7 @@ import { motion } from "framer-motion";
 
 const Logo = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="45"
-      height="40"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="40">
       <text
         id="secondM"
         x="5"
@@ -32,9 +28,9 @@ const Logo = () => {
         fontSize="40"
         fill="white"
         style={{ zIndex: "2" }}
-        initial={{x: "0px", y: "0px"}}
-        whileHover={{x: "5px", y: "-4px"}}
-        transition={{type: "tween"}}
+        initial={{ x: "0px", y: "0px" }}
+        whileHover={{ x: "5px", y: "-4px" }}
+        transition={{ type: "tween" }}
       >
         M
       </motion.text>
